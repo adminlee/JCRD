f5a8f0805f76683a0ea5817a5b1948ae91a73722
biubiubiumin@foxmail.com <caodaning1>
2020-03-06 09:56:11 +0800

    “massage”

---
+++ contents/composer.js
@@ -0,0 +1,22 @@
+{
+    "name": "adminlee/JCRD",
+	"description":"file_upload",
+    "type": "library",
+	"license": "MIT",
+    "require": {
+        "php": ">=5.6.0",
+		"topthink/framework": "^5.0",
+    },
+    "authors": [
+        {
+            "name": "adminLee",
+            "email": "biubiubiumin@foxmail.com"
+        }
+    ],
+    "autoload": {
+        "psr-4": {
+            "JCRD": "src"
+        }
+    },
+     
+}
