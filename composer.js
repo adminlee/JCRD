c129e1f7211a9f49a2ff1ecd5bb1ee00621d8dd7
biubiubiumin@foxmail.com <caodaning1>
2020-03-06 10:10:49 +0800

    “massage”

--- composer.js
+++ composer.js
@@ -1,22 +1,22 @@
 {
     "name": "adminlee/JCRD",
 	"description":"file_upload",
+	"keywords": ["thinkphp","file"],
     "type": "library",
 	"license": "MIT",
-    "require": {
-        "php": ">=5.6.0",
-		"topthink/framework": "^5.0",
-    },
     "authors": [
         {
             "name": "adminLee",
             "email": "biubiubiumin@foxmail.com"
         }
-    ],
+    ], 
+	"require": {
+		"topthink/framework": "^5.0"
+    }, 
     "autoload": {
         "psr-4": {
             "JCRD": "src"
         }
-    },
+    }
      
 }
